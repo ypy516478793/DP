fe439d7ead90eccbef58efdcad4de099b14d703e
Pengyu <pyuan2@central.uh.edu>
2018-03-02 14:57:05 -0600

    AC_MDP e_greedy without_terminal_function

--- AC_MDP.py
+++ AC_MDP.py
@@ -13,7 +13,8 @@ gym 0.8.0
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import gym
+import pandas as pd
+from MDP_env import MDP_env
 from collections import defaultdict
 
 np.random.seed(2)
@@ -23,18 +24,21 @@ tf.set_random_seed(2)  # reproducible
 OUTPUT_GRAPH = False
 MAX_EPISODE = 3000
 DISPLAY_REWARD_THRESHOLD = 200  # renders environment if total episode reward is greater then this threshold
-MAX_EP_STEPS = 1000   # maximum time step in one episode
+MAX_EP_STEPS = 1000  # maximum time step in one episode
 RENDER = False  # rendering wastes time
-GAMMA = 0.9     # reward discount in TD error
-LR_A = 0.001    # learning rate for actor
-LR_C = 0.01     # learning rate for critic
+GAMMA = 0.9  # reward discount in TD error
+LR_A = 0.001  # learning rate for actor
+LR_C = 0.01  # learning rate for critic
 
-env = gym.make('CartPole-v0')
-env.seed(1)  # reproducible
-env = env.unwrapped
+# env = gym.make('CartPole-v0')
+# env.seed(1)  # reproducible
+# env = env.unwrapped
+env = MDP_env()
 
-N_F = env.observation_space.shape[0]
-N_A = env.action_space.n
+# N_F = env.observation_space.shape[0]
+# N_A = env.action_space.n
+N_F = 6
+N_A = 2
 N_O = (4, 8)
 
 
@@ -50,22 +54,20 @@ class Actor(object):
         self.a = tf.placeholder(tf.int32, None, "act")
         self.td_error = tf.placeholder(tf.float32, None, "td_error")  # TD_error
 
-
-
         with tf.variable_scope('Actor'):
             l1 = tf.layers.dense(
                 inputs=self.s,
-                units=20,    # number of hidden units
+                units=20,  # number of hidden units
                 activation=tf.nn.relu,
-                kernel_initializer=tf.random_normal_initializer(0., .1),    # weights
+                kernel_initializer=tf.random_normal_initializer(0., .1),  # weights
                 bias_initializer=tf.constant_initializer(0.1),  # biases
                 name='l1'
             )
 
             self.acts_prob = tf.layers.dense(
                 inputs=l1,
-                units=n_actions,    # output units
-                activation=tf.nn.softmax,   # get action probabilities
+                units=n_actions,  # output units
+                activation=tf.nn.softmax,  # get action probabilities
                 kernel_initializer=tf.random_normal_initializer(0., .1),  # weights
                 bias_initializer=tf.constant_initializer(0.1),  # biases
                 name='acts_prob'
@@ -86,15 +88,15 @@ class Actor(object):
 
     def choose_action(self, s):
         s = s[np.newaxis, :]
-        probs = self.sess.run(self.acts_prob, {self.s: s})   # get probabilities for all actions
-        return np.random.choice(np.arange(probs.shape[1]), p=probs.ravel())   # return a int
-    
+        probs = self.sess.run(self.acts_prob, {self.s: s})  # get probabilities for all actions
+        return np.random.choice(np.arange(probs.shape[1]), p=probs.ravel())  # return a int
+
     def eval_options_probs(self, Q):
         options_probs = defaultdict(list)
         for i in range(2):
             options_probs[i] = np.ones(self.n_options[i]) * self.epsilon[i] / self.n_options[i]
             options_probs[i][np.argmax(Q[i])] += 1 - self.epsilon[i]
-        return  options_probs
+        return options_probs
 
     def choose_option(self, l, Q):
         options_probs = self.eval_options_probs(Q)
@@ -103,8 +105,8 @@ class Actor(object):
 
     def eval_option_term(self, l, Q_):
         options_probs_next = self.eval_options_probs(Q_)
-        options_term_prob_next = 1 - options_probs_next[l-1]
-        return options_term_prob_next[self.chosen_options[l-1]]
+        options_term_prob_next = 1 - options_probs_next[l - 1]
+        return options_term_prob_next[self.chosen_options[l - 1]]
 
     def anneal(self, l):
         if self.epsilon[l] > 0.1:
@@ -116,7 +118,7 @@ class Critic(object):
         self.sess = sess
         self.n_options = n_options
 
-        self.s = tf.placeholder(tf.float32, [1, n_features], "state")
+        self.s = tf.placeholder(tf.float32, [None, n_features], "state")
         self.o = tf.placeholder(tf.int32, [None, len(n_options)], "chosen_options")
         self.v_ = tf.placeholder(tf.float32, None, "v_next")
         self.r = tf.placeholder(tf.float32, None, 'r')
@@ -158,7 +160,7 @@ class Critic(object):
         with tf.variable_scope('squared_TD_error'):
             self.chosen_v = tf.gather_nd(self.v, self.options_indexer, "chosen_V")
             self.td_error = self.r + self.v_ - self.chosen_v
-            self.loss = tf.square(self.td_error)    # TD_error = (r+gamma*V_next) - V_eval
+            self.loss = tf.square(self.td_error)  # TD_error = (r+gamma*V_next) - V_eval
         with tf.variable_scope('train'):
             self.train_op = tf.train.AdamOptimizer(lr).minimize(self.loss)
 
@@ -166,11 +168,11 @@ class Critic(object):
         s, s_ = s[np.newaxis, :], s_[np.newaxis, :]
         o = o[np.newaxis, :]
         td_error, _ = self.sess.run([self.td_error, self.train_op],
-                                          {self.s: s, self.v_: v_, self.r: r, self.o: o})
+                                    {self.s: s, self.v_: v_, self.r: r, self.o: o})
         return td_error
-    
+
     def eval_q(self, state, chosen_options=None):
-        state= state[np.newaxis, :]
+        state = state[np.newaxis, :]
         Q_options = self.sess.run(self.v, {self.s: state})
         Q_options = np.ravel(Q_options)
         hierarchical_q = self.reshape(Q_options)
@@ -186,9 +188,10 @@ class Critic(object):
         hierarchical_q = defaultdict(list)
         pointer = 0
         for i in range(2):
-            hierarchical_q[i] = Q[pointer: pointer+self.n_options[i]]
+            hierarchical_q[i] = Q[pointer: pointer + self.n_options[i]]
             pointer += self.n_options[i]
         return hierarchical_q
+
 
 def plot_figures():
     plt.figure(1)
@@ -196,12 +199,22 @@ def plot_figures():
     plt.xlabel("i_episode")
     plt.ylabel("running_reward")
 
+def output_q_tables():
+    # all_state = np.arange(n_states).reshape((n_states, 1))
+    all_state = np.eye(N_F)
+    q_table = pd.DataFrame(index=list(range(N_F)), columns=list(range(sum(N_O))), dtype=np.float64)
+    q_table.iloc[:,:] = critic.sess.run(critic.v, feed_dict={critic.s: all_state})
+    q_table['action'] = q_table.idxmax(axis=1)
+    print("")
+    print(" q_table:")
+    print(q_table.sort_index(axis=0, ascending=True))
 
 
 sess = tf.Session()
 
 actor = Actor(sess, n_features=N_F, n_options=N_O, n_actions=N_A, lr=LR_A)
-critic = Critic(sess, n_features=N_F, n_options=N_O, lr=LR_C)     # we need a good teacher, so the teacher should learn faster than the actor
+critic = Critic(sess, n_features=N_F, n_options=N_O,
+                lr=LR_C)  # we need a good teacher, so the teacher should learn faster than the actor
 
 sess.run(tf.global_variables_initializer())
 
@@ -210,6 +223,8 @@ if OUTPUT_GRAPH:
 
 reward_list = []
 
+visits = np.zeros([N_O[0],N_O[1],N_A])
+
 for i_episode in range(MAX_EPISODE):
     s = env.reset()
     t = 0
@@ -217,7 +232,7 @@ for i_episode in range(MAX_EPISODE):
     done = False
 
     while not done and t < MAX_EP_STEPS:
-        if RENDER: env.render()
+        # if RENDER: env.render()
         option_done_0 = False
         hierarchical_Q = critic.eval_q(s)
         actor.choose_option(0, hierarchical_Q)
@@ -229,13 +244,13 @@ for i_episode in range(MAX_EPISODE):
 
             while not option_done_1 and not done:
 
-
-
                 a = actor.choose_action(s)
 
-                s_, r, done, info = env.step(a)
+                s_, r, done = env.step(a)
 
-                print("state:", s, "  options", actor.chosen_options, "  action", a)
+                visits[actor.chosen_options[0],actor.chosen_options[1],a] += 1
+
+                print("episode:", i_episode, "  state:", np.argmax(s), "  options", actor.chosen_options, "  action", a)
 
                 hierarchical_Q_next = critic.eval_q(s_)
 
@@ -245,15 +260,14 @@ for i_episode in range(MAX_EPISODE):
                 if done:
                     v_ = 0
                 else:
-                    U = (1 - beta1) * hierarchical_Q_next[0][actor.chosen_options[0]] + beta1 * np.max(hierarchical_Q_next[0])
+                    U = (1 - beta1) * hierarchical_Q_next[0][actor.chosen_options[0]] + beta1 * np.max(
+                        hierarchical_Q_next[0])
                     v_ = GAMMA * (1 - beta2) * hierarchical_Q_next[1][actor.chosen_options[1]] + GAMMA * beta2 * U
-
-                if done: r = -20
 
                 track_r.append(r)
 
                 td_error = critic.learn(s, r, s_, v_, actor.chosen_options)  # gradient = grad[r + gamma * V(s_) - V(s)]
-                actor.learn(s, a, td_error)     # true_gradient = grad[logPi(s,a) * td_error]
+                actor.learn(s, a, td_error)  # true_gradient = grad[logPi(s,a) * td_error]
 
                 s = s_
                 t += 1
@@ -267,18 +281,7 @@ for i_episode in range(MAX_EPISODE):
 
     actor.anneal(0)
 
-
-
-
-
-
-
-
-
-
     ep_rs_sum = sum(track_r)
-
-
 
     if 'running_reward' not in globals():
         running_reward = ep_rs_sum
@@ -286,7 +289,9 @@ for i_episode in range(MAX_EPISODE):
     else:
         running_reward = running_reward * 0.95 + ep_rs_sum * 0.05
         reward_list.append(running_reward)
-    if running_reward > DISPLAY_REWARD_THRESHOLD: RENDER = True  # rendering
+    # if running_reward > DISPLAY_REWARD_THRESHOLD: RENDER = True  # rendering
     print("episode:", i_episode, "  reward:", int(running_reward), "  epsilon", actor.epsilon)
+
+print("")
 
 
--- MDP_env.py
+++ MDP_env.py
@@ -1,36 +1,49 @@
 import random
+import numpy as np
+
+random.seed(1)
 
 class MDP_env:
     def __init__(self):
         self.visited_six = False
         self.current_state = 1
+        self.state = one_hot(self.current_state)
 
     def reset(self):
         self.visited_six = False
         self.current_state = 1
-        return self.current_state
+        self.state = one_hot(self.current_state)
+        return self.state
 
     def step(self, action):
         if self.current_state != 0:
             # If "right" selected
             if action == 1:
-                if self.current_state < 5:  # if current_state == 5, do not change current_state
-                    self.current_state += 1
-                # if random.random() < 0.5:
-                #     if self.current_state < 5:  # if current_state == 5, do not change current_state
-                #         self.current_state += 1
-                # else:
-                #     self.current_state -= 1
+                # if self.current_state < 5:  # if current_state == 5, do not change current_state
+                #     self.current_state += 1
+                if random.random() < 0.5:
+                    if self.current_state < 5:  # if current_state == 5, do not change current_state
+                        self.current_state += 1
+                else:
+                    self.current_state -= 1
             # If "left" selected
             if action == 0:
                 self.current_state -= 1
             # If state 6 reached
             if self.current_state == 5:
                 self.visited_six = True
+
+        self.state = one_hot(self.current_state)
+
         if self.current_state == 0:
             if self.visited_six:
-                return self.current_state, 1.00, True
+                return self.state, 1.00, True
             else:
-                return self.current_state, 1.00/100.00, True
+                return self.state, 1.00/100.00, True
         else:
-            return self.current_state, 0.0, False
+            return self.state, 0.0, False
+
+def one_hot(state):
+    state_vector = np.zeros(6)
+    state_vector[state] = 1
+    return state_vector
